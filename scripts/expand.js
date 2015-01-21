6163f58db2be9a0cc93e5f4948c12d82f11ad697
whonore <whonore@u.rochester.edu>
2015-01-21 17:24:56 -0500

    Added projects

---
+++ scripts/expand.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $("button").click(function() {
+    toggleIcon($(this).find("span"));
+    $("#" + this.name).toggle(500);
+  });
+  
+  /* Start off collapsed */
+  $("button").each(function() {
+    $(this).trigger("click");
+  });
+});
+
+function toggleIcon(icon) {
+  switch (icon.text()) {
+    case "+":
+      icon.text("-");
+      break;
+    case "-":
+      icon.text("+");
+      break;
+  }
+}
